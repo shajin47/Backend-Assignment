65c044d79dd7b179a750a6b3452c4e0a7d61652a
shajin47 <105574749+shajin47@users.noreply.github.com>
2024-01-13 12:04:55 +0530

    Update doubtRoutes.js

--- doubt_share/routes/doubtRoutes.js
+++ doubt_share/routes/doubtRoutes.js
@@ -1,11 +1,24 @@
 const router = require('express').Router()
-const bcrypt = require('bcrypt')
 const Doubt = require('../models/doubtModel');
-const jwt = require('jsonwebtoken')
 const authMiddleware = require('../middlewares/authMiddleware')
 
 
-//resgiter a user
+//all-doubts
+
+router.get('/all-doubts' ,authMiddleware, async (req , res)=>{
+    try {
+        const studentId = req.body.userId
+        const doubts = await Doubt.find({ student : studentId})
+          .sort( {createdAt : -1})
+          .exec();
+        res.json(doubts);
+    } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+//add doubts
 
 router.post('/add',authMiddleware, async (req, res) => {
     try {
@@ -19,19 +32,6 @@ router.post('/add',authMiddleware, async (req, res) => {
     }
 })
 
-//login Routes
-
-router.get('/all-doubts' ,authMiddleware, async (req , res)=>{
-    try {
-        const doubts = await Doubt.find({})
-          .sort({ timestamp: -1 })
-          .exec();
-        res.json(doubts);
-    } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-    }
-})
 
 
 module.exports = router
