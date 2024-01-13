572deaac71398002f2d114864f40c0b732302328
shajin47 <105574749+shajin47@users.noreply.github.com>
2024-01-13 12:18:12 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AllMyDoubts from './Components/AllMyDoubts';
+import TutorAvailabilityUpdater from './Components/TutorAvailabilityUpdater';
+
+function App() {
+  return (
+    <div className="App">
+      <TutorAvailabilityUpdater></TutorAvailabilityUpdater>
+      <AllMyDoubts></AllMyDoubts>
+    </div>
+  );
+}
+
+export default App;
